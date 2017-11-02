097ea780d17eeb76ee1a871fcceeb6a42d3b2570
Varun Sundar <varun19299@gmail.com>
2017-11-02 12:02:04 +0530

    Waveforms for ALU,CU

---
+++ test_ALU.v
@@ -0,0 +1,99 @@
+`timescale 1ns / 1ps
+
+/*
+Testbench for RISC module
+*/
+`include "Parameter.v"
+`include "ALU.v"
+
+module test_ALU;
+
+ // Inputs
+ reg [7:0] a;
+ reg  [7:0] b;
+ reg [2:0] cnt;
+ wire [7:0] c;
+ wire zero;
+
+ // Instantiate the Unit Under Test (UUT)
+ ALU uut (a,b,cnt,c,zero
+ );
+
+ initial
+  begin
+
+  $dumpfile("./Waveforms/test_ALU.vcd");
+  $dumpvars(0, a,b,cnt,c,zero);
+
+   a=8'd0;
+   b=8'd0;
+   cnt=3'd0;
+
+   #20 begin
+   a=8'd2;
+   b=8'd5;
+   cnt=3'd0;
+   end
+
+   #30 begin
+   a=8'd10;
+   b=8'd13;
+   cnt=3'd0;
+   end
+
+   #40 begin
+
+   a=8'd2;
+   b=8'd1;
+   cnt=3'd1;
+
+   end
+
+   #50 begin
+   a=8'd10;
+   b=8'd13;
+   cnt=3'd2;
+   end
+
+   #60 begin
+   a=8'd10;
+   b=8'd13;
+   cnt=3'd3;
+   end
+
+   #70 begin
+   a=8'd10;
+   b=8'd13;
+   cnt=3'd4;
+   end
+
+   #80 begin
+   a=8'd10;
+   b=8'd13;
+   cnt=3'd5;
+   end
+
+   #90 begin
+   a=8'd10;
+   b=8'd13;
+   cnt=3'd6;
+   end
+
+   #100 begin
+   a=8'd10;
+   b=8'd13;
+   cnt=3'd7;
+   end
+
+   #110 begin
+   a=8'd2;
+   b=8'd5;
+   cnt=3'd0;
+   end
+
+
+   `simulation_time;
+   $finish;
+  end
+
+endmodule
---
+++ test_CU.v
@@ -0,0 +1,86 @@
+`timescale 1ns / 1ps
+
+/*
+Testbench for RISC module
+*/
+`include "Parameter.v"
+`include "CU.v"
+
+module test_ALU;
+
+ // Inputs
+ reg [3:0] opcode;
+ wire [1:0] alu_op;
+ wire jump,beq,bne,mem_read,mem_write,alu_src,reg_dst,mem_to_reg,reg_write;
+
+
+ // Instantiate the Unit Under Test (UUT)
+ Control_Unit uut (opcode,alu_op,jump,beq,bne,mem_read,mem_write,
+   alu_src,reg_dst,mem_to_reg,reg_write);
+
+ initial
+  begin
+
+  $dumpfile("./Waveforms/test_CU.vcd");
+  $dumpvars(0, opcode,alu_op,jump,beq,bne,mem_read,mem_write,
+    alu_src,reg_dst,mem_to_reg,reg_write);
+
+   opcode=4'd0;
+
+   #20 begin
+   opcode=4'd0;
+   end
+
+   #30 begin
+   opcode=4'd1;
+   end
+
+   #40 begin
+   opcode=4'd2;
+   end
+
+   #50 begin
+   opcode=4'd3;
+   end
+
+   #60 begin
+   opcode=4'd4;
+   end
+
+   #70 begin
+   opcode=4'd5;
+   end
+
+   #80 begin
+   opcode=4'd6;
+   end
+
+   #90 begin
+   opcode=4'd7;
+   end
+
+   #100 begin
+   opcode=4'd8;
+   end
+
+   #110 begin
+   opcode=4'd9;
+   end
+
+   #120 begin
+   opcode=4'd11;
+   end
+
+   #130 begin
+   opcode=4'd12;
+   end
+
+   #140 begin
+   opcode=3'd13;
+   end
+
+   `simulation_time;
+   $finish;
+  end
+
+endmodule
